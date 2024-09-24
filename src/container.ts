650003576452bda5e9ac31ed029c893c642582ef
rafaelbrandtbalmant <balmantrafael@gmail.com>
2024-09-24 17:21:13 -0300

    _-_-_-_-_><_-_-_-_-_-_-_

--- src/container.ts
+++ src/container.ts
@@ -2,12 +2,30 @@ import { UserService } from '@services/user.service'
 import { UserRepository } from '@repositories/user.repository'
 import { UserModel } from '@models/user.model'
 import { Sequelize } from 'sequelize'
+import winston from 'winston'
 
 const sequelize = new Sequelize('postgres://root:root@localhost:5432/holygod')
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'MAIN CONTAINER' },
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+    ],
+})
 
 const container = async () => {
     try {
         await sequelize.authenticate()
+        console.log('testeeee')
+        logger.info(
+            `[CONTAINER ${
+                new Date().toTimeString
+            } - Sequelize Connected to Database`
+        )
 
         const models = {
             userModel: new UserModel(sequelize),
@@ -24,7 +42,9 @@ const container = async () => {
         return {
             services,
         }
-    } catch (e) {}
+    } catch (e) {
+        logger.error(e)
+    }
 }
 
 export default container
